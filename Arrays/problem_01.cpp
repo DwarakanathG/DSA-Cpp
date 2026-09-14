330e168991d6ace764c724e7ac1a7199ae6a6df7
Dwarakanath9151 <dg9151@srmist.edu.in>
2026-09-14 19:18:42 +0530

    Two sum

--- Arrays/problem_01.cpp
+++ Arrays/problem_01.cpp
@@ -1,3 +1,5 @@
+//problem 1. Two Sum
+
 #include <vector>
 #include <unordered_map>
 using namespace std;
@@ -20,3 +22,6 @@ public:
         return {};
     }
 };
+
+//time complexity: O(n) - The code iterates through the input array once, performing constant-time operations for each element, resulting in linear time complexity.
+//space complexity: O(n) - The code uses an unordered_map to store the elements of the input array, which can potentially store all n elements in the worst case, leading to linear space complexity.
