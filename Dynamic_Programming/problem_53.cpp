b11fc1b9ef2f9d2e29b1030381b4441e94339fd1
Dwarakanath9151 <dg9151@srmist.edu.in>
2026-09-16 17:34:22 +0530

    Number of Ways to Draw N Non-Overlapping Line Segments

---
+++ Dynamic_Programming/problem_53.cpp
@@ -0,0 +1,42 @@
+//Problem: 1621. Number of Ways to Draw N Non-Overlapping Line Segments
+
+class Solution {
+private:
+    const int MOD = 1e9 + 7;
+    long long power(long long base, long long exp){
+        long long res = 1;
+        base %= MOD;
+        while(exp > 0){
+            if(exp % 2 == 1) res =(res * base) % MOD;
+            base = (base * base) % MOD;
+            exp /= 2;
+        }
+        return(res);
+    }
+
+    long long modInverse(long long n) {
+        return power(n, MOD - 2);
+    }
+
+    long long nCr(int n, int r) {
+        if (r < 0 || r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        if (r > n - r) r = n - r; // Optimize symmetry
+
+        long long num = 1, den = 1;
+        for (int i = 0; i < r; i++) {
+            num = (num * (n - i)) % MOD;
+            den = (den * (i + 1)) % MOD;
+        }
+
+        return((num * modInverse(den)) % MOD);
+    }
+
+public:
+    int numberOfSets(int n, int k) {
+        return(nCr(n + k -1, 2 * k));
+    }
+};
+
+//Time Complexity: O(k) for calculating nCr
+//Space Complexity: O(1) for using constant space
