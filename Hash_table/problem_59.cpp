e9fcb6f5c8b5fbd1d2770c6e602a6a4eaec19aaa
Dwarakanath9151 <dg9151@srmist.edu.in>
2026-09-18 20:50:59 +0530

    Linked List Cycle II

---
+++ Hash_table/problem_59.cpp
@@ -0,0 +1,36 @@
+//Problem 142: Linked List Cycle II
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next) return(nullptr);
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+        
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return(slow);
+            }
+        }
+        return(nullptr);
+    }
+};
+
+//Time Complexity: O(n) for traversing the linked list
+//Space Complexity: O(1) for using two pointers
